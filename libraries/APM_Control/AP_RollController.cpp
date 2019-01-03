6209a90bd619b3de2fdc52d62465bb5c0f83a99f
Mark Whitehorn <kd0aij@gmail.com>
2019-01-03 14:30:24 -0700

    APM_Control: fix indentation

--- libraries/APM_Control/AP_RollController.cpp
+++ libraries/APM_Control/AP_RollController.cpp
@@ -84,85 +84,84 @@ const AP_Param::GroupInfo AP_RollController::var_info[] = {
 	AP_GROUPEND
 };
 
-
 /*
   internal rate controller, called by attitude and rate controller
   public functions
-*/
-int32_t AP_RollController::_get_rate_out(float desired_rate, float scaler, bool disable_integrator)
+ */
+int32_t AP_RollController::_get_rate_out(float desired_rate, float scaler, bool disable_integrator) 
 {
-	uint32_t tnow = AP_HAL::millis();
-	uint32_t dt = tnow - _last_t;
-	if (_last_t == 0 || dt > 1000) {
-		dt = 0;
-	}
-	_last_t = tnow;
-	
-	// Calculate equivalent gains so that values for K_P and K_I can be taken across from the old PID law
+    uint32_t tnow = AP_HAL::millis();
+    uint32_t dt = tnow - _last_t;
+    if (_last_t == 0 || dt > 1000) {
+        dt = 0;
+    }
+    _last_t = tnow;
+
+    // Calculate equivalent gains so that values for K_P and K_I can be taken across from the old PID law
     // No conversion is required for K_D
-	float ki_rate = gains.I * gains.tau;
+    float ki_rate = gains.I * gains.tau;
     float eas2tas = _ahrs.get_EAS2TAS();
-	float kp_ff = MAX((gains.P - gains.I * gains.tau) * gains.tau  - gains.D , 0) / eas2tas;
+    float kp_ff = MAX((gains.P - gains.I * gains.tau) * gains.tau - gains.D, 0) / eas2tas;
     float k_ff = gains.FF / eas2tas;
-	float delta_time    = (float)dt * 0.001f;
-	
-	// Limit the demanded roll rate
-	if (gains.rmax && desired_rate < -gains.rmax) {
-        desired_rate = - gains.rmax;
+    float delta_time = (float) dt * 0.001f;
+
+    // Limit the demanded roll rate
+    if (gains.rmax && desired_rate < -gains.rmax) {
+        desired_rate = -gains.rmax;
     } else if (gains.rmax && desired_rate > gains.rmax) {
         desired_rate = gains.rmax;
     }
-	
+
     // Get body rate vector (radians/sec)
-	float omega_x = _ahrs.get_gyro().x;
-	
-	// Calculate the roll rate error (deg/sec) and apply gain scaler
+    float omega_x = _ahrs.get_gyro().x;
+
+    // Calculate the roll rate error (deg/sec) and apply gain scaler
     float achieved_rate = ToDeg(omega_x);
-	float rate_error = (desired_rate - achieved_rate) * scaler;
-	
-	// Get an airspeed estimate - default to zero if none available
-	float aspeed;
-	if (!_ahrs.airspeed_estimate(&aspeed)) {
+    float rate_error = (desired_rate - achieved_rate) * scaler;
+
+    // Get an airspeed estimate - default to zero if none available
+    float aspeed;
+    if (!_ahrs.airspeed_estimate(&aspeed)) {
         aspeed = 0.0f;
     }
 
-	// Multiply roll rate error by _ki_rate, apply scaler and integrate
-	// Scaler is applied before integrator so that integrator state relates directly to aileron deflection
-	// This means aileron trim offset doesn't change as the value of scaler changes with airspeed
-	// Don't integrate if in stabilise mode as the integrator will wind up against the pilots inputs
-	if (!disable_integrator && ki_rate > 0) {
-		//only integrate if gain and time step are positive and airspeed above min value.
-		if (dt > 0 && aspeed > float(aparm.airspeed_min)) {
-		    float integrator_delta = rate_error * ki_rate * delta_time * scaler;
-			// prevent the integrator from increasing if surface defln demand is above the upper limit
-			if (_last_out < -45) {
-                integrator_delta = MAX(integrator_delta , 0);
+    // Multiply roll rate error by _ki_rate, apply scaler and integrate
+    // Scaler is applied before integrator so that integrator state relates directly to aileron deflection
+    // This means aileron trim offset doesn't change as the value of scaler changes with airspeed
+    // Don't integrate if in stabilise mode as the integrator will wind up against the pilots inputs
+    if (!disable_integrator && ki_rate > 0) {
+        //only integrate if gain and time step are positive and airspeed above min value.
+        if (dt > 0 && aspeed > float(aparm.airspeed_min)) {
+            float integrator_delta = rate_error * ki_rate * delta_time * scaler;
+            // prevent the integrator from increasing if surface defln demand is above the upper limit
+            if (_last_out < -45) {
+                integrator_delta = MAX(integrator_delta, 0);
             } else if (_last_out > 45) {
                 // prevent the integrator from decreasing if surface defln demand  is below the lower limit
-                 integrator_delta = MIN(integrator_delta, 0);
+                integrator_delta = MIN(integrator_delta, 0);
             }
-			_pid_info.I += integrator_delta;
-		}
-	} else {
-		_pid_info.I = 0;
-	}
-	
+            _pid_info.I += integrator_delta;
+        }
+    } else {
+        _pid_info.I = 0;
+    }
+
     // Scale the integration limit
     float intLimScaled = gains.imax * 0.01f;
 
     // Constrain the integrator state
     _pid_info.I = constrain_float(_pid_info.I, -intLimScaled, intLimScaled);
-	
-	// Calculate the demanded control surface deflection
-	// Note the scaler is applied again. We want a 1/speed scaler applied to the feed-forward
-	// path, but want a 1/speed^2 scaler applied to the rate error path. 
-	// This is because acceleration scales with speed^2, but rate scales with speed.
+
+    // Calculate the demanded control surface deflection
+    // Note the scaler is applied again. We want a 1/speed scaler applied to the feed-forward
+    // path, but want a 1/speed^2 scaler applied to the rate error path. 
+    // This is because acceleration scales with speed^2, but rate scales with speed.
     _pid_info.D = rate_error * gains.D * scaler;
     _pid_info.P = desired_rate * kp_ff * scaler;
     _pid_info.FF = desired_rate * k_ff * scaler;
     _pid_info.desired = desired_rate;
 
-	_last_out = _pid_info.FF + _pid_info.P + _pid_info.D;
+    _last_out = _pid_info.FF + _pid_info.P + _pid_info.D;
 
     if (autotune.running && aspeed > aparm.airspeed_min) {
         // let autotune have a go at the values 
@@ -172,10 +171,10 @@ int32_t AP_RollController::_get_rate_out(float desired_rate, float scaler, bool 
         autotune.update(desired_rate, achieved_rate, _last_out);
     }
 
-	_last_out += _pid_info.I;
-	
-	// Convert to centi-degrees and constrain
-	return constrain_float(_last_out * 100, -4500, 4500);
+    _last_out += _pid_info.I;
+
+    // Convert to centi-degrees and constrain
+    return constrain_float(_last_out * 100, -4500, 4500);
 }
 
 
