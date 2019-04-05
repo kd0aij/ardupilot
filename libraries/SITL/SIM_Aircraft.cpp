1b47095773e0b558bde25df85bd3d4616e5eea48
IamPete1 <33176108+IamPete1@users.noreply.github.com>
2019-04-05 19:28:09 +0100

    SITL: add coppter tail sitter

--- libraries/SITL/SIM_Aircraft.cpp
+++ libraries/SITL/SIM_Aircraft.cpp
@@ -570,10 +570,10 @@ void Aircraft::update_dynamics(const Vector3f &rot_accel)
             break;
         }
         case GROUND_BEHAVIOR_TAILSITTER: {
-            // point straight up
+            // point almost straight up
             float r, p, y;
             dcm.to_euler(&r, &p, &y);
-            dcm.from_euler(0.0f, radians(90), y);
+            dcm.from_euler(0.0f, radians(85), y);
             // no movement
             if (accel_earth.z > -1.1*GRAVITY_MSS) {
                 velocity_ef.zero();
--- libraries/SITL/SIM_QuadPlane.cpp
+++ libraries/SITL/SIM_QuadPlane.cpp
@@ -28,7 +28,9 @@ QuadPlane::QuadPlane(const char *home_str, const char *frame_str) :
     // default to X frame
     const char *frame_type = "x";
     uint8_t motor_offset = 4;
-    
+
+    ground_behavior = GROUND_BEHAVIOR_NO_MOVEMENT;
+
     if (strstr(frame_str, "-octa-quad")) {
         frame_type = "octa-quad";
     } else if (strstr(frame_str, "-octaquad")) {
@@ -67,6 +69,12 @@ QuadPlane::QuadPlane(const char *home_str, const char *frame_str) :
         frame_type = "tilttri";
         // fwd motor gives zero thrust
         thrust_scale = 0;
+    } else if (strstr(frame_str, "-copter_tailsitter")) {
+        frame_type = "+";
+        tailsitter = true;
+        copter_tailsitter = true;
+        ground_behavior = GROUND_BEHAVIOR_TAILSITTER;
+        thrust_scale *= 1.5;
     }
     frame = Frame::find_frame(frame_type);
     if (frame == nullptr) {
@@ -88,7 +96,6 @@ QuadPlane::QuadPlane(const char *home_str, const char *frame_str) :
     // we use zero terminal velocity to let the plane model handle the drag
     frame->init(mass, 0.51, 0, 0);
 
-    ground_behavior = GROUND_BEHAVIOR_NO_MOVEMENT;
 }
 
 /*
@@ -108,6 +115,12 @@ void QuadPlane::update(const struct sitl_input &input)
     Vector3f quad_accel_body;
 
     frame->calculate_forces(*this, input, quad_rot_accel, quad_accel_body);
+
+    // rotate frames for copter tailsitters
+    if (copter_tailsitter) {
+        quad_rot_accel.rotate(ROTATION_PITCH_270);
+        quad_accel_body.rotate(ROTATION_PITCH_270);
+    }
 
     // estimate voltage and current
     frame->current_and_voltage(input, battery_voltage, battery_current);
