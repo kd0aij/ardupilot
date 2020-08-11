efb3dfde902737ff64cfb58f7324fce64977cf76
Mark Whitehorn <kd0aij@gmail.com>
2020-08-11 17:36:07 -0600

    AP_Motors: add AP_MotorsGeneral

---
+++ libraries/AP_Motors/AP_MotorsGeneral.h
@@ -0,0 +1,117 @@
+/// @file	AP_MotorsGeneral.h
+/// @brief	Motor control class for arbitrary vectored thrust configurations
+#pragma once
+
+#include <AP_Common/AP_Common.h>
+#include <AP_Math/AP_Math.h>           // ArduPilot Vector/Matrix math Library
+#include <RC_Channel/RC_Channel.h>     // RC Channel Library
+#include "AP_MotorsMulticopter.h"
+
+#define AP_MOTORS_MATRIX_YAW_FACTOR_CW   -1
+#define AP_MOTORS_MATRIX_YAW_FACTOR_CCW   1
+
+// tail servo uses channel 7
+#define AP_MOTORS_CH_TRI_YAW    CH_7
+#define AP_MOTORS_TRI_SERVO_RANGE_DEG_MIN   5   // minimum angle movement of tail servo in degrees
+#define AP_MOTORS_TRI_SERVO_RANGE_DEG_MAX   80  // maximum angle movement of tail servo in degrees
+
+/// @class      AP_MotorsGeneral
+class AP_MotorsGeneral : public AP_MotorsMulticopter {
+public:
+
+    /// Constructor
+    AP_MotorsGeneral(uint16_t loop_rate, uint16_t speed_hz = AP_MOTORS_SPEED_DEFAULT) :
+        AP_MotorsMulticopter(loop_rate, speed_hz)
+    {};
+
+    // init
+    void                init(motor_frame_class frame_class, motor_frame_type frame_type) override;
+
+    // set frame class (i.e. quad, hexa, heli) and type (i.e. x, plus)
+    void                set_frame_class_and_type(motor_frame_class frame_class, motor_frame_type frame_type) override;
+
+    // set update rate to motors - a value in hertz
+    // you must have setup_motors before calling this
+    void                set_update_rate(uint16_t speed_hz) override;
+
+    // output_test_seq - spin a motor at the pwm value specified
+    //  motor_seq is the motor's sequence number from 1 to the number of motors on the frame
+    //  pwm value is an actual pwm value that will be output, normally in the range of 1000 ~ 2000
+    virtual void        output_test_seq(uint8_t motor_seq, int16_t pwm) override;
+
+    // output_test_num - spin a motor connected to the specified output channel
+    //  (should only be performed during testing)
+    //  If a motor output channel is remapped, the mapped channel is used.
+    //  Returns true if motor output is set, false otherwise
+    //  pwm value is an actual pwm value that will be output, normally in the range of 1000 ~ 2000
+    bool                output_test_num(uint8_t motor, int16_t pwm);
+
+    // output_to_motors - sends minimum values out to the motors
+    void                output_to_motors() override;
+
+    // get_motor_mask - returns a bitmask of which outputs are being used for motors (1 means being used)
+    //  this can be used to ensure other pwm outputs (i.e. for servos) do not conflict
+    uint16_t            get_motor_mask() override;
+
+    // output a thrust to all motors that match a given motor
+    // mask. This is used to control tiltrotor motors in forward
+    // flight. Thrust is in the range 0 to 1
+    // rudder_dt applys diffential thrust for yaw in the range 0 to 1
+    void                output_motor_mask(float thrust, uint8_t mask, float rudder_dt) override;
+
+    // return number of motor that has failed.  Should only be called if get_thrust_boost() returns true
+    uint8_t             get_lost_motor() const override { return _motor_lost_index; }
+
+    // TODO: return the roll factor of any motor, this is used for tilt rotors and tail sitters
+    // using copter motors for forward flight
+    float               get_roll_factor(uint8_t i) override { return 0.0f; }
+
+    // return the location and direction of any motor
+    Vector3f            get_loc(uint8_t i) { return _loc[i]; }
+    Vector3f            get_dir(uint8_t i) { return _dir[i]; }
+
+protected:
+    // output - sends commands to the motors
+    void                output_armed_stabilizing() override;
+
+    // check for failed motor
+    void                check_for_failed_motor(float throttle_thrust_best);
+
+    // add_motor using raw roll, pitch, throttle and yaw factors
+    void                add_motor_raw(int8_t motor_num, float roll_fac, float pitch_fac, float yaw_fac, uint8_t testing_order);
+
+    // add_motor using just position and yaw_factor (or prop direction)
+    void                add_motor(int8_t motor_num, float angle_degrees, float yaw_factor, uint8_t testing_order);
+
+    // add_motor using separate roll and pitch factors (for asymmetrical frames) and prop direction
+    void                add_motor(int8_t motor_num, float roll_factor_in_degrees, float pitch_factor_in_degrees, float yaw_factor, uint8_t testing_order);
+
+    // remove_motor
+    void                remove_motor(int8_t motor_num);
+
+    // configures the motors for the defined frame_class and frame_type
+    virtual void        setup_motors(motor_frame_class frame_class, motor_frame_type frame_type);
+
+    // normalizes the roll, pitch and yaw factors so maximum magnitude is 0.5
+    void                normalise_rpy_factors();
+
+    // call vehicle supplied thrust compensation if set
+    void                thrust_compensation(void) override;
+
+    Vector3f            _loc[AP_MOTORS_MAX_NUM_MOTORS]; // each motor's location
+    Vector3f            _dir[AP_MOTORS_MAX_NUM_MOTORS]; // each motor's thrust direction
+    //TODO: _yaw_factor is currently used by the 2 NoYawTorque frame types to disable use of motor torque for yaw control
+    // this becomes a continuous instead of binary decision in the general case where yaw torques are (possibly) constrained and balanced
+    // the same as for roll and pitch
+    float               _yaw_factor[AP_MOTORS_MAX_NUM_MOTORS]; // each motor's rotation direction
+    SRV_Channel         *_yaw_servo;    // yaw output channel
+    float               _pivot_angle;   // Angle of yaw pivot
+    float               _thrust_rpyt_out[AP_MOTORS_MAX_NUM_MOTORS]; // combined roll, pitch, yaw and throttle outputs to motors in 0~1 range
+    uint8_t             _test_order[AP_MOTORS_MAX_NUM_MOTORS];  // order of the motors in the test sequence
+    motor_frame_class   _last_frame_class; // most recently requested frame class (i.e. quad, hexa, octa, etc)
+    motor_frame_type    _last_frame_type; // most recently requested frame type (i.e. plus, x, v, etc)
+
+    // motor failure handling
+    float               _thrust_rpyt_out_filt[AP_MOTORS_MAX_NUM_MOTORS];    // filtered thrust outputs with 1 second time constant
+    uint8_t             _motor_lost_index;  // index number of the lost motor
+};
